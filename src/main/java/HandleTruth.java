3ef5d0a7ec9bb2e1ea77800c7626a8f5b80dbb7f
Nabiya <alamnabiya@gmail.com>
2020-11-19 23:05:16 -0800

    Minor code cleanup

--- src/main/java/HandleTruth.java
+++ src/main/java/HandleTruth.java
@@ -1,8 +1,7 @@
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class HandleTruth {
-    public static TreeMap<Integer, Set<String>> wordCount(String msg) {
+    public static Map<Integer, Set<String>> wordCount(String msg) {
         String[] words = msg.split(" ");
 
         // use map to count the occurrences of each word
@@ -14,16 +13,13 @@ public class HandleTruth {
             }
             wordMap.put(word, 1);
         }
-//        for (Map.Entry<String, Integer> entry : wordMap.entrySet()) {
-//            System.out.println(entry);
-//        }
 
         return getSortedMap(wordMap);
     }
 
     // sort the map based on number of occurrences
-    protected static TreeMap<Integer, Set<String>> getSortedMap(HashMap<String, Integer> wordMap) {
-        TreeMap<Integer, Set<String>> sortedMap = new TreeMap<>(Collections.reverseOrder());
+    protected static Map<Integer, Set<String>> getSortedMap(HashMap<String, Integer> wordMap) {
+        Map<Integer, Set<String>> sortedMap = new TreeMap<>(Collections.reverseOrder());
         for (Map.Entry<String, Integer> entry : wordMap.entrySet()) {
             int key = entry.getValue();
             Set<String> values = sortedMap.containsKey(key) ?
--- src/test/java/HandleTruthTest.java
+++ src/test/java/HandleTruthTest.java
@@ -7,12 +7,10 @@ import static org.junit.Assert.assertEquals;
 public class HandleTruthTest {
     @Test
     public void makeshiftTest() {
-      //  String msg = "son we live in a world that has walls and those walls have to be guarded by men with guns whos gonna do it you you lieutenant weinberg i have a greater responsibility than you can possibly fathom you weep for santiago and you curse the marines you have that luxury you have the luxury of not knowing what i know that santiagos death while tragic probably saved lives and my existence while grotesque and incomprehensible to you saves lives you dont want the truth because deep down in places you dont talk about at parties you want me on that wall you need me on that wall we use words like honor code loyalty we use these words as the backbone of a life spent defending something you use them as a punchline i have neither the time nor the inclination to explain myself to a man who rises and sleeps under the blanket of the very freedom that i provide and then questions the manner in which i provide it i would rather you just said thank you and went on your way otherwise i suggest you pick up a weapon and stand a post either way i dont give a damn what you think you are entitled to";
-        //TreeMap<Integer, Set<String>> result = HandleTruth.wordCount(msg);
         Map<String, TreeMap<Integer, Set<String>>> testCases = getTestCases();
 
         for (Map.Entry<String, TreeMap<Integer, Set<String>>> entry : testCases.entrySet()) {
-            TreeMap<Integer, Set<String>> actual = HandleTruth.wordCount(entry.getKey());
+            Map<Integer, Set<String>> actual = HandleTruth.wordCount(entry.getKey());
             assertEquals(entry.getValue(), actual);
         }
     }
@@ -32,7 +30,6 @@ public class HandleTruthTest {
         expected.put(2, new HashSet<>(Arrays.asList("lives", "want", "words", "it", "while", "way", "as", "walls", "what", "provide", "me", "luxury", "wall")));
         expected.put(1, new HashSet<>(Arrays.asList("explain", "very", "guns", "saved", "lieutenant", "about", "your", "these", "saves", "would", "pick", "fathom", "grotesque", "incomprehensible", "because", "greater", "give", "sleeps", "went", "loyalty", "myself", "them", "then", "something", "at", "knowing", "santiago", "curse", "weinberg", "entitled", "whos", "be", "probably", "world", "freedom", "responsibility", "are", "by", "tragic", "backbone", "man", "stand", "inclination", "think", "gonna", "suggest", "places", "thank", "under", "punchline", "honor", "existence", "defending", "questions", "do", "manner", "down", "either", "son", "post", "than", "talk", "has", "up", "those", "possibly", "which", "otherwise", "need", "like", "my", "parties", "know", "santiagos", "rises", "who", "deep", "code", "death", "rather", "for", "weep", "life", "nor", "guarded", "can", "weapon", "not", "men", "said", "just", "live", "spent", "damn", "with", "truth", "marines", "neither", "time", "blanket")));
         testcases.put(key, expected);
-
 
         // Test 2
         key = "";
